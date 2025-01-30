32f053d7ea4162734cadd2b723a53f2964bd4925
Godwin Peter .O <me@godwin.dev>
2025-01-30 14:34:25 +0100

    Update osm_pull.sh
    Signed-off-by: Godwin Peter .O <me@godwin.dev>

--- osm_pull.sh
+++ osm_pull.sh
@@ -1,4 +1,4 @@
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
-pip install --upgrade osmium boto3
-python pull_osm_data.py
+python3 -m pip install --upgrade osmium boto3
+python3 pull_osm_data.py
